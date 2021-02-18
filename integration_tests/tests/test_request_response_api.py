b8abedb120b25c307a3c27d2e5e564cf1b190923
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2021-02-18 15:50:35 -0500

    Debug e2e testing in CI

--- integration_tests/tests/test_request_response_api.py
+++ integration_tests/tests/test_request_response_api.py
@@ -1,45 +1,45 @@
-# """Tests for endpoints on the Request & Response API."""
-# # pylint: disable=redefined-outer-name
-# # pylint: disable=no-method-argument
-# # pylint: disable=missing-function-docstring
-#
-# from typing import Tuple
-#
-# import pytest
-#
-# from helpers.rpc_client import Client, Connection, Channel
-#
-#
-# # create connection objects and make available for the module scope
-# connection_and_channel = pytest.mark.usefixtures('connection_and_channel')
-#
-#
-# @pytest.fixture
-# def client(
-#     connection_and_channel: Tuple[Connection, Channel]
-# ) -> Client:
-#     """Setup an RPC client from test helper module."""
-#     return Client(*connection_and_channel)
-#
-#
-# class TestRouteTest:
-#     """Tests for API endpoint `test`."""
-#
-#     @staticmethod
-#     def test_response_should_be_successful(client: Client) -> None:
-#         successful = client.call('test', 'message')['success']
-#
-#         assert successful
-#
-#     @staticmethod
-#     def test_response_appends_that_took_forever_to_message(
-#             client: Client
-#     ) -> None:
-#         data = client.call('test', 'message')['data']
-#
-#         assert data == 'message that took forever'
-#
-#
+"""Tests for endpoints on the Request & Response API."""
+# pylint: disable=redefined-outer-name
+# pylint: disable=no-method-argument
+# pylint: disable=missing-function-docstring
+
+from typing import Tuple
+
+import pytest
+
+from helpers.rpc_client import Client, Connection, Channel
+
+
+# create connection objects and make available for the module scope
+connection_and_channel = pytest.mark.usefixtures('connection_and_channel')
+
+
+@pytest.fixture
+def client(
+    connection_and_channel: Tuple[Connection, Channel]
+) -> Client:
+    """Setup an RPC client from test helper module."""
+    return Client(*connection_and_channel)
+
+
+class TestRouteTest:
+    """Tests for API endpoint `test`."""
+
+    @staticmethod
+    def test_response_should_be_successful(client: Client) -> None:
+        successful = client.call('test', 'message')['success']
+
+        assert successful
+
+    @staticmethod
+    def test_response_appends_that_took_forever_to_message(
+            client: Client
+    ) -> None:
+        data = client.call('test', 'message')['data']
+
+        assert data == 'message that took forever'
+
+
 # class TestRouteWillError:
 #     """Tests for API endpoint `will-error`."""
 #
