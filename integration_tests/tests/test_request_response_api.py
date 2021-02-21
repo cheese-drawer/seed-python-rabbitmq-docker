9534d0f0e4e156d27195e1f31193110a5f1b8f8c
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2021-02-21 11:27:37 -0500

    Debug e2e testing in CI

--- integration_tests/tests/test_request_response_api.py
+++ integration_tests/tests/test_request_response_api.py
@@ -1,51 +1,69 @@
-# """Tests for endpoints on the Request & Response API."""
-# # pylint: disable=redefined-outer-name
-# # pylint: disable=no-method-argument
-# # pylint: disable=missing-function-docstring
-# # pylint: disable=too-few-public-methods
-#
-# from typing import Tuple
-#
-# import pytest
-#
-# from helpers.rpc_client import Client, Connection, Channel
-#
-#
-# # create connection objects and make available for the module scope
+"""Tests for endpoints on the Request & Response API."""
+# pylint: disable=redefined-outer-name
+# pylint: disable=no-method-argument
+# pylint: disable=missing-function-docstring
+# pylint: disable=too-few-public-methods
+
+from typing import Any, Generator, Tuple
+
+import pytest
+
+from helpers.connection import connect, Connection, Channel
+from helpers.rpc_client import Client
+
+
+# create connection objects and make available for the module scope
 # connection_and_channel = pytest.mark.usefixtures('connection_and_channel')
-#
-#
-# @pytest.fixture
-# def client(
-#     connection_and_channel: Tuple[Connection, Channel]
-# ) -> Client:
-#     """Setup an RPC client from test helper module."""
-#     print('setting up client...')
-#     return Client(*connection_and_channel)
-#
-#
-# class TestRouteTest:
-#     """Tests for API endpoint `test`."""
-#
-#     @staticmethod
-#     def test_response_should_be_successful(client: Client) -> None:
-#         print('running test_response_should_be_successful')
-#
-#         successful = client.call('test', 'message')['success']
-#
-#         assert successful
-#
-#     @staticmethod
-#     def test_response_appends_that_took_forever_to_message(
-#             client: Client
-#     ) -> None:
-#         print('running test_response_appends_that_took_forever_to_message')
-#
-#         data = client.call('test', 'message')['data']
-#
-#         assert data == 'message that took forever'
-#
-#
+
+
+def connection_and_channel(
+) -> Tuple[Connection, Channel]:
+    """Set up & tears down a connection to the test broker."""
+    connection, channel = connect(
+        host='localhost',
+        port=8672,
+        user='test',
+        password='pass'
+    )
+
+    return connection, channel
+
+
+@pytest.fixture
+def client(
+) -> Generator[Client, Any, Any]:
+    """Setup an RPC client from test helper module."""
+    print('setting up client...')
+
+    connection, channel = connection_and_channel()
+
+    yield Client(connection, channel)
+
+    connection.close()
+
+
+class TestRouteTest:
+    """Tests for API endpoint `test`."""
+
+    @staticmethod
+    def test_response_should_be_successful(client: Client) -> None:
+        print('running test_response_should_be_successful')
+
+        successful = client.call('test', 'message')['success']
+
+        assert successful
+
+    @staticmethod
+    def test_response_appends_that_took_forever_to_message(
+            client: Client
+    ) -> None:
+        print('running test_response_appends_that_took_forever_to_message')
+
+        data = client.call('test', 'message')['data']
+
+        assert data == 'message that took forever'
+
+
 # class TestRouteWillError:
 #     """Tests for API endpoint `will-error`."""
 #
